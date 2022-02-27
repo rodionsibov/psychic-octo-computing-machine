f4a55f23fad8c7c55a3cf917e56a8d86f02bbba0
Rodion Sibov <rodionsibov@gmail.com>
2022-02-27 07:46:15 +0200

    first commit

---
+++ src/components/Guide.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+    <div class="page">
+        <h1>Guide</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, odit?</p>
+        <h2>Lorem, ipsum.</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa, repudiandae nemo repellendus voluptate eligendi, magni hic rerum, necessitatibus reiciendis dolor aspernatur ab voluptatem accusantium ea eius molestias excepturi. Soluta, dolore?</p>
+    </div>
+</template>
