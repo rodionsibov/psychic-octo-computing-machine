57a9a87ac37cb30d1a524395f0be858d4770775d
Rodion Sibov <rodionsibov@gmail.com>
2022-02-28 00:30:17 +0200

    update

--- src/components/DragAndDrop.vue
+++ src/components/DragAndDrop.vue
@@ -1,11 +1,39 @@
 <script setup>
+import { ref } from "@vue/reactivity";
 
+const items = ref([
+  {
+    id: 0,
+    title: "Item A",
+    list: 1
+  },
+  {
+    id: 1,
+    title: "Item B",
+    list: 1
+  },
+  {
+    id: 2,
+    title: "Item C",
+    list: 2
+  },
+]);
+
+const getList = (list) => {
+  return items.value.filter((item) => item.list == list);
+};
 </script>
 
 <template>
-  <h1>drag and drop</h1>
+  <div>
+    <h1>drag and drop</h1>
+    <div class="drop-zone">
+      <div v-for="item in getList" :key="item.id">
+        {{ item.title }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
 </style>
